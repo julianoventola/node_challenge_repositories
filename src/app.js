f6ee69398fc1b9a0b8ec0deaaa897d86fc5434c4
JulianoV <ventolajuliano@gmail.com>
2020-04-07 23:28:37 -0300

    feat: put route to edit a repository

--- src/app.js
+++ src/app.js
@@ -22,7 +22,17 @@ app.post('/repositories', (request, response) => {
 })
 
 app.put('/repositories/:id', (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  )
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+  const { likes } = repositories[repositoryIndex]
+  repositories[repositoryIndex] = { id, title, url, techs, likes }
+  return response.json(repositories)
 })
 
 app.delete('/repositories/:id', (req, res) => {
