dd24c3f4c0ddd742347bd23530b6be6e30cf2917
JulianoV <ventolajuliano@gmail.com>
2020-04-07 23:20:17 -0300

    feat: post route to add new repository

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
+app.get('/repositories', (request, response) => {
   // TODO
-});
+})
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body
+  const newRepository = { id: uuid(), title, url, techs, likes: 0 }
+  repositories.push(newRepository)
+  return response.json(newRepository)
+})
+
+app.put('/repositories/:id', (request, response) => {
   // TODO
-});
+})
 
-app.put("/repositories/:id", (request, response) => {
+app.delete('/repositories/:id', (req, res) => {
   // TODO
-});
+})
 
-app.delete("/repositories/:id", (req, res) => {
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
-});
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
-
-module.exports = app;
+module.exports = app
